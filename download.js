2497cb748a2b4a90cb19e844beec80a90cd3864d
Julian Beck <ju-fa-beck@t-online.de>
2019-02-20 20:19:38 +0100

    add sanitize-filename

--- download.js
+++ download.js
@@ -21,135 +21,86 @@ let options;
  */
 async function download(dir, config, ytVideo) {
 
-    if (typeof dir == undefined) throw "please specify an directory";
-    directory = dir;
+	if (typeof dir == undefined) throw "please specify an directory";
+	directory = dir;
 
-    //set default value when calling as a module
-    options = Object.assign({
-        startAt: 0,
-        endAt: 0,
-        duration: 180,
-        full: true,
-    }, config);
+	//set default value when calling as a module
+	options = Object.assign({
+		startAt: 0,
+		endAt: 0,
+		duration: 180,
+		full: true,
+	}, config);
 
-    //parse time stamps to seconds
-    options.startAt = stringToSeconds(options.startAt);
-    options.endAt = stringToSeconds(options.endAt);
-    options.duration = stringToSeconds(options.duration);
+	//parse time stamps to seconds
+	options.startAt = stringToSeconds(options.startAt);
+	options.endAt = stringToSeconds(options.endAt);
+	options.duration = stringToSeconds(options.duration);
 
-    //sets path variables for ffmpeg
-    await checkffmpeg();
+	//sets path variables for ffmpeg
+	await checkffmpeg();
 
-    //Download yt videos
-    if (isUrl(ytVideo)) {
+	//Download yt videos
 
-        if (directory.indexOf("https://www.youtube.com/") >= 0) {
-            let youtubeDir = path.join(options.output, "YouTube");
-            //run get playlist
-            let videos
 
-            //check if single video or playlist
-            try {
-                videos = await getPlaylist(directory);
-                if (videos.length == 0) videos = [directory];
-            } catch (error) {
-                console.log(error);
-                videos = [directory];
-            }
+	if (ytdl.validateURL(ytVideo)) {
+		let title = await getVideoTitle(video);
+		title = title.replace(/[/\\?%*:|"<>&]/g, "-"); //make sure there are no illeagale characters
+		await downloadVideo(video, path.join(youtubeDir, title + ".mp4"));
+		i++;
+	}
 
-            urlSpinner.succeed(`Found ${chalk.blue(videos.length)} YouTube video(s)`);
-            fs.mkdirSync(youtubeDir);
 
-            const spinner = ora(`downloading ${chalk.blue(videos.length)} video(s)...`).start();
-            let i = 1;
-            for (let video of videos) {
-                if (ytdl.validateURL(video)) {
-                    let title = await getVideoTitle(video);
-                    title = title.replace(/[/\\?%*:|"<>&]/g, "-"); //make sure there are no illeagale characters
-                    spinner.text = `downloading ${chalk.blue(title)}, video ${chalk.blue(i)}/${chalk.blue(videos.length)}`;
-                    await downloadVideo(video, path.join(youtubeDir, title + ".mp4"));
-                    i++;
-                }
-            }
-            spinner.succeed(`downloaded ${chalk.blue(videos.length)} video(s)`);
-            //set directory to youtubeDir
-            directory = youtubeDir;
-        } else {
-            //check for podcast
-            let rss
-            try {
-                rss = await getRSS(directory);
-            } catch (error) {
-                urlSpinner.fail(`Could not detect podcast rss feed or YouTube link`);
-                throw (directory + " is not a YouTube or RSS feed url");
-            }
-            //create podcast output folder
-            urlSpinner.succeed(`Found ${chalk.blue(rss.episodes.length)} Podcast episodes`);
-            let podcastDir = path.join(options.output, "Podcast");
-            fs.mkdirSync(podcastDir);
-            let episodes = options.podcastLimit == 0 ? rss.episodes : rss.episodes.slice(0, options.podcastLimit);
-            const downloadSpinner = ora(`Start downloading ${chalk.blue(episodes.length)} episodes(s)...`).start();
-            let i = 0;
-            for (const podcast of episodes) {
-                i++;
-                downloadSpinner.text = `downloading ${chalk.blue(podcast.title)}, podcast ${chalk.blue(i)}/${chalk.blue(episodes.length)}`;
-                await downloadAudio(path.join(podcastDir, podcast.title.replace(/[/\\?%*:|"<>&]/g, "-") + ".mp3"), podcast.enclosure.url);
-            }
-            downloadSpinner.succeed(`downloaded ${chalk.blue(episodes.length)} Podcast episode(s)`);
 
-            //set main directory to podcast
-            directory = podcastDir
-        }
 
-    }
-    //get files
-    const fileSpinner = ora(`searching for files...`).start();
-    let files = getFiles(directory);
-    //check if files are media files
-    files = verifyFiles(files);
-    if (typeof files === "undefined" || files.length == 0) {
-        fileSpinner.fail("no file where found inside " + directory)
-        throw "no files where found inside " + directory;
-    }
-    fileSpinner.succeed(`found ${chalk.blue(files.length)} file(s), start splitting...`)
+	//get files
+	const fileSpinner = ora(`searching for files...`).start();
+	let files = getFiles(directory);
+	//check if files are media files
+	files = verifyFiles(files);
+	if (typeof files === "undefined" || files.length == 0) {
+		fileSpinner.fail("no file where found inside " + directory)
+		throw "no files where found inside " + directory;
+	}
+	fileSpinner.succeed(`found ${chalk.blue(files.length)} file(s), start splitting...`)
 
-    //rename files
-    files = options.rename ? rename(files) : files;
-    let baseDirectory = path.dirname(files[0]);
-    let outputDirectory = path.join(baseDirectory, "audio");
+	//rename files
+	files = options.rename ? rename(files) : files;
+	let baseDirectory = path.dirname(files[0]);
+	let outputDirectory = path.join(baseDirectory, "audio");
 
-    //create folders, delete existing files
-    if (!fs.existsSync(outputDirectory))
-        fs.mkdirSync(outputDirectory);
+	//create folders, delete existing files
+	if (!fs.existsSync(outputDirectory))
+		fs.mkdirSync(outputDirectory);
 
-    //Split track
-    for (let item of files) {
-        let seconds = await getFileLength(item);
-        let filename = path.basename(item);
-        await splitTrack(baseDirectory, outputDirectory, filename, Number(seconds));
-    }
+	//Split track
+	for (let item of files) {
+		let seconds = await getFileLength(item);
+		let filename = path.basename(item);
+		await splitTrack(baseDirectory, outputDirectory, filename, Number(seconds));
+	}
 
-    //set metadata name to first file in array if not set
-    if (options.name === "") {
-        let filename = path.basename(files[0])
-        options.name = filename.substr(0, filename.lastIndexOf(".")) || filename;
-    }
-    //take cover picture
-    let cover = "";
-    if (options.cover)
-        coverPath = await getCoverPicture(files[0], baseDirectory, options.startAt);
+	//set metadata name to first file in array if not set
+	if (options.name === "") {
+		let filename = path.basename(files[0])
+		options.name = filename.substr(0, filename.lastIndexOf(".")) || filename;
+	}
+	//take cover picture
+	let cover = "";
+	if (options.cover)
+		coverPath = await getCoverPicture(files[0], baseDirectory, options.startAt);
 
-    //updating meta data
-    if (options.metadata) {
-        const metadataSpinner = ora(`searching for files...`).start();
-        files = fs.readdirSync(outputDirectory);
-        for (let file of files) {
-            await writeMusicMetadata(path.join(outputDirectory, file), options.name, coverPath);
-        }
-        metadataSpinner.succeed(`updated metadata of ${chalk.blue(files.length)} file(s)`);
-    }
+	//updating meta data
+	if (options.metadata) {
+		const metadataSpinner = ora(`searching for files...`).start();
+		files = fs.readdirSync(outputDirectory);
+		for (let file of files) {
+			await writeMusicMetadata(path.join(outputDirectory, file), options.name, coverPath);
+		}
+		metadataSpinner.succeed(`updated metadata of ${chalk.blue(files.length)} file(s)`);
+	}
 
-    if (options.cover) await deleteFile(coverPath);
+	if (options.cover) await deleteFile(coverPath);
 }
 
 
@@ -158,13 +109,13 @@ async function download(dir, config, ytVideo) {
  * packages that require it
  */
 function checkffmpeg() {
-    ffmpeg.setFfmpegPath(ffmpegPath);
-    ffmpeg.setFfprobePath(ffprobePath);
-    process.env.FFMPEG_PATH = ffmpegPath;
-    ffprobe.FFPROBE_PATH = ffprobePath;
-    ffmetadata = require("ffmetadata");
-    console.log(chalk.grey("ffmpeg installed at:" + ffmpegPath));
-    return ffmpegPath;
+	ffmpeg.setFfmpegPath(ffmpegPath);
+	ffmpeg.setFfprobePath(ffprobePath);
+	process.env.FFMPEG_PATH = ffmpegPath;
+	ffprobe.FFPROBE_PATH = ffprobePath;
+	ffmetadata = require("ffmetadata");
+	console.log(chalk.grey("ffmpeg installed at:" + ffmpegPath));
+	return ffmpegPath;
 }
 
 /**
@@ -177,14 +128,14 @@ function checkffmpeg() {
  * @param {Number} duration 
  */
 function segmentMp3(input, output, start, duration) {
-    return new Promise((resolve, reject) => {
-        ffmpeg(input).seekInput(start).duration(duration).save(output)
-            .on("end", function (stdout, stderr) {
-                resolve();
-            }).on('error', function (err, stdout, stderr) {
-                reject('Cannot process video: ' + err.message);
-            });
-    });
+	return new Promise((resolve, reject) => {
+		ffmpeg(input).seekInput(start).duration(duration).save(output)
+			.on("end", function (stdout, stderr) {
+				resolve();
+			}).on('error', function (err, stdout, stderr) {
+				reject('Cannot process video: ' + err.message);
+			});
+	});
 };
 
 
@@ -197,32 +148,32 @@ function segmentMp3(input, output, start, duration) {
  * @param {Number} duration 
  */
 async function splitTrack(baseDirectory, outputDirectory, name, duration) {
-    let parts = 0;
-    const spinner = ora(`splitting ${name} into ${chalk.blue(parts + 1)} parts`).start();
+	let parts = 0;
+	const spinner = ora(`splitting ${name} into ${chalk.blue(parts + 1)} parts`).start();
 
-    //if you dont want seprate clips
-    if (options.full) {
-        let ext = path.extname(name);
-        let newName = path.removeExt(name, ext);
-        await segmentMp3(path.join(baseDirectory, name), path.join(outputDirectory, newName + ".mp3"), 0, duration);
-        spinner.succeed(`Converted ${chalk.blue(newName)} into mp3`);
-        return;
-    }
+	//if you dont want seprate clips
+	if (options.full) {
+		let ext = path.extname(name);
+		let newName = path.removeExt(name, ext);
+		await segmentMp3(path.join(baseDirectory, name), path.join(outputDirectory, newName + ".mp3"), 0, duration);
+		spinner.succeed(`Converted ${chalk.blue(newName)} into mp3`);
+		return;
+	}
 
-    let durationIndex = options.startAt;
+	let durationIndex = options.startAt;
 
-    while ((durationIndex + options.duration) <= (duration - options.endAt)) {
-        spinner.text = `splitting ${name} into ${chalk.blue(parts + 1)} parts`;
-        await segmentMp3(path.join(baseDirectory, name), path.join(outputDirectory, getSegmentName(name, durationIndex, durationIndex + options.duration)), durationIndex, options.duration);
-        durationIndex += options.duration;
-        parts++;
-    }
-    if (((duration - options.endAt) - durationIndex) >= 30) {
-        spinner.text = `splitting ${name} into ${chalk.blue(parts + 1)} parts`;
-        await segmentMp3(path.join(baseDirectory, name), path.join(outputDirectory, getSegmentName(name, durationIndex, duration - options.endAt)), durationIndex, ((duration - options.endAt) - durationIndex));
-        parts++;
-    }
-    spinner.succeed(`splitted ${name} into ${chalk.blue(parts)} parts`);
+	while ((durationIndex + options.duration) <= (duration - options.endAt)) {
+		spinner.text = `splitting ${name} into ${chalk.blue(parts + 1)} parts`;
+		await segmentMp3(path.join(baseDirectory, name), path.join(outputDirectory, getSegmentName(name, durationIndex, durationIndex + options.duration)), durationIndex, options.duration);
+		durationIndex += options.duration;
+		parts++;
+	}
+	if (((duration - options.endAt) - durationIndex) >= 30) {
+		spinner.text = `splitting ${name} into ${chalk.blue(parts + 1)} parts`;
+		await segmentMp3(path.join(baseDirectory, name), path.join(outputDirectory, getSegmentName(name, durationIndex, duration - options.endAt)), durationIndex, ((duration - options.endAt) - durationIndex));
+		parts++;
+	}
+	spinner.succeed(`splitted ${name} into ${chalk.blue(parts)} parts`);
 
 }
 
@@ -234,9 +185,9 @@ async function splitTrack(baseDirectory, outputDirectory, name, duration) {
  * @param {Number} end 
  */
 function getSegmentName(name, start, end) {
-    let ext = path.extname(name);
-    name = path.removeExt(name, ext);
-    return `${name}_${secondsToTimeString(start)}-${secondsToTimeString(end)}.mp3`.replace(/[/\\?%*:|"<>&]/g, '-');
+	let ext = path.extname(name);
+	name = path.removeExt(name, ext);
+	return `${name}_${secondsToTimeString(start)}-${secondsToTimeString(end)}.mp3`.replace(/[/\\?%*:|"<>&]/g, '-');
 }
 
 
@@ -245,7 +196,7 @@ function getSegmentName(name, start, end) {
  * @param {Number} seconds 
  */
 function secondsToTimeString(seconds) {
-    return new Date(seconds * 1000).toISOString().substr(14, 5).replace(":", ".");
+	return new Date(seconds * 1000).toISOString().substr(14, 5).replace(":", ".");
 
 }
 
@@ -254,18 +205,18 @@ function secondsToTimeString(seconds) {
  * @param {String} timeString 
  */
 function stringToSeconds(timeString) {
-    let seconds = 0;
-    if (!isNaN(timeString))
-        seconds = timeString;
-    else if (typeof timeString === "string" || timeString instanceof String) {
-        if (timeString.indexOf(":") > -1) {
-            let ms = timeString.split(":");
-            seconds = (+ms[0]) * 60 + (+ms[1]);
-        }
-    } else
-        throw timeString + " is not a number, please only use formats like 123 or 1:30";
+	let seconds = 0;
+	if (!isNaN(timeString))
+		seconds = timeString;
+	else if (typeof timeString === "string" || timeString instanceof String) {
+		if (timeString.indexOf(":") > -1) {
+			let ms = timeString.split(":");
+			seconds = (+ms[0]) * 60 + (+ms[1]);
+		}
+	} else
+		throw timeString + " is not a number, please only use formats like 123 or 1:30";
 
-    return Number(seconds);
+	return Number(seconds);
 }
 
 
@@ -275,12 +226,12 @@ function stringToSeconds(timeString) {
  * @param {*} file 
  */
 function getFileLength(file) {
-    return new Promise((resolve, reject) => {
-        ffprobe(file, (err, probeData) => {
-            if (err) reject(err);
-            resolve(probeData.format.duration);
-        });
-    });
+	return new Promise((resolve, reject) => {
+		ffprobe(file, (err, probeData) => {
+			if (err) reject(err);
+			resolve(probeData.format.duration);
+		});
+	});
 }
 
 
@@ -292,26 +243,26 @@ function getFileLength(file) {
  * @param {String} cover 
  */
 function writeMusicMetadata(file, compilationName, cover) {
-    return new Promise((resolve, reject) => {
+	return new Promise((resolve, reject) => {
 
-        let isodate = new Date();
-        let data = {
-            artist: compilationName,
-            genre: "speech",
-            disc: 1,
-            album: compilationName,
-            date: isodate
-        };
+		let isodate = new Date();
+		let data = {
+			artist: compilationName,
+			genre: "speech",
+			disc: 1,
+			album: compilationName,
+			date: isodate
+		};
 
-        let attachments = options.cover ? {
-            attachments: [cover]
-        } : {};
+		let attachments = options.cover ? {
+			attachments: [cover]
+		} : {};
 
-        ffmetadata.write(file, data, attachments, function (err) {
-            if (err) reject(err);
-            resolve();
-        });
-    });
+		ffmetadata.write(file, data, attachments, function (err) {
+			if (err) reject(err);
+			resolve();
+		});
+	});
 }
 
 /**
@@ -319,14 +270,14 @@ function writeMusicMetadata(file, compilationName, cover) {
  * @param {*} file 
  */
 function deleteFile(file) {
-    return new Promise((resolve, reject) => {
-        fs.unlink(file, function (error) {
-            if (error) {
-                reject(error);
-            }
-            resolve();
-        });
-    });
+	return new Promise((resolve, reject) => {
+		fs.unlink(file, function (error) {
+			if (error) {
+				reject(error);
+			}
+			resolve();
+		});
+	});
 }
 
 
@@ -337,12 +288,12 @@ function deleteFile(file) {
  * @param {String} dir where the video should be placed
  */
 async function downloadVideo(url, dir) {
-    return new Promise((resolve, reject) => {
-        ytdl(url)
-            .pipe(fs.createWriteStream(dir)).on("finish", () => {
-                resolve(dir);
-            });
-    });
+	return new Promise((resolve, reject) => {
+		ytdl(url)
+			.pipe(fs.createWriteStream(dir)).on("finish", () => {
+				resolve(dir);
+			});
+	});
 }
 
 
@@ -351,12 +302,12 @@ async function downloadVideo(url, dir) {
  * @param {} url 
  */
 async function getVideoTitle(url) {
-    return new Promise((resolve, reject) => {
-        ytdl.getInfo(url, (err, info) => {
-            if (err) throw reject(err);
-            resolve(info.title);
-        })
-    });
+	return new Promise((resolve, reject) => {
+		ytdl.getInfo(url, (err, info) => {
+			if (err) throw reject(err);
+			resolve(info.title);
+		})
+	});
 }
 
 
