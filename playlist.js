2f9f6e15085a4ec6617f473a99e36de576cd4588
Julian Beck <ju-fa-beck@t-online.de>
2019-02-20 16:05:08 +0100

    pl wrapper

---
+++ playlist.js
@@ -0,0 +1,17 @@
+const ytpl = require('ytpl');
+
+async function getPlaylistInformation(url,limit){
+    return await getPlaylist(url,limit);
+}   
+module.exports.getPlaylistInformation = getPlaylistInformation
+
+
+function getPlaylist(url,limit) {
+    return new Promise((resolve, reject) => {
+        ytpl(url,{limit:limit}, function(err, playlist) {
+            if(err) throw reject(err);
+            resolve(playlist)
+
+          });
+    });
+}
