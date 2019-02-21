aaed70e96d39991fc5b5b4a8b5ddda7ea709b64d
Julian Beck <ju-fa-beck@t-online.de>
2019-02-21 12:00:04 +0100

    minor changes

--- cli.js
+++ cli.js
@@ -3,7 +3,32 @@
 const dl = require("./download");
 const db = require("./db")
 const fs = require("fs")
+const init = require("./init")
 const path = require("upath")
+const meow = require('meow');
+
+const cli = meow(`
+	Usage
+	  $ myapp <input>
+
+	Options
+	  --init, -i  create new Directory
+	  --update <mins>, -u <min>	 update every <min> mins
+
+	Examples
+	  $ myapp unicorns --init
+`, {
+	flags: {
+		init: {
+			type: 'boolean',
+			alias: 'i'
+		},
+		update: {
+			type: 'string',
+			alias: 'u'
+		}
+	}
+});
 
 
 const basePath = "./test"
@@ -30,6 +55,15 @@ async function update() {
  * call vitomuci, pass over args, opions and argv
  */
 (async () => {
+	console.log(cli.input[0], cli.flags)
+	if (cli.input[0] === undefined) {
+		console.log("Pleace specify a path")
+		return
+	}
+	if (cli.flags.init) {
+		init(cli.input[0]);
+	}
+	
 	let rawdata = fs.readFileSync("test/config.json");
 	let config = JSON.parse(rawdata);
 	db.init(path.join(basePath, "db.json"));
